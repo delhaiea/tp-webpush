90af6994d4408b718b4801751176897bba221152
Alexis Delhaie <roboform@hotmail.fr>
2020-10-19 10:06:54 +0200

    Create service-worker

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -1,3 +1,12 @@
 function checkBrowser() {
     return (('serviceWorker' in navigator) && ('PushManager' in window))
 }
+
+function registerServiceWorker() {
+    return navigator.serviceWorker.register("javascripts/service-worker.js").then((reg) => {
+        console.log('Service worker registered');
+        return reg;
+    }).catch((err) => {
+        console.error("ptdr sa crash: " + err);
+    })
+}
